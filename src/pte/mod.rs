c7a09ae56796caf84ed8616bd9bab0fcf5b92f99
Godones <1925466036@qq.com>
2023-06-13 18:39:07 +0800

    feat: import new page-table impl
    from Arceos

---
+++ src/pte/mod.rs
@@ -0,0 +1,47 @@
+mod riscv;
+
+use crate::addr::PhysAddr;
+use core::fmt::Debug;
+
+pub use self::riscv::*;
+
+bitflags::bitflags! {
+    /// Generic page table entry flags that indicate the corresponding mapped
+    /// memory region permissions and attributes.
+    pub struct MappingFlags: usize {
+        /// The memory is readable.
+        const READ          = 1 << 0;
+        /// The memory is writable.
+        const WRITE         = 1 << 1;
+        /// The memory is executable.
+        const EXECUTE       = 1 << 2;
+        /// The memory is user accessible.
+        const USER          = 1 << 3;
+        /// The memory is device memory.
+        const DEVICE        = 1 << 4;
+    }
+}
+
+/// A generic page table entry.
+///
+/// All architecture-specific page table entry types implement this trait.
+pub trait GenericPTE: Debug + Clone + Copy + Sync + Send + Sized {
+    /// Creates a page table entry point to a terminate page or block.
+    fn new_page(paddr: PhysAddr, flags: MappingFlags, is_huge: bool) -> Self;
+    /// Creates a page table entry point to a next level page table.
+    fn new_table(paddr: PhysAddr) -> Self;
+
+    /// Returns the physical address mapped by this entry.
+    fn paddr(&self) -> PhysAddr;
+    /// Returns the flags of this entry.
+    fn flags(&self) -> MappingFlags;
+    /// Returns whether this entry is zero.
+    fn is_unused(&self) -> bool;
+    /// Returns whether this entry flag indicates present.
+    fn is_present(&self) -> bool;
+    /// For non-last level translation, returns whether this entry maps to a
+    /// huge frame.
+    fn is_huge(&self) -> bool;
+    /// Set this entry to zero.
+    fn clear(&mut self);
+}
